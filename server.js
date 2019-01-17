4b850e279005b8c0f6f1765f4ecaad1caeeafb61
Michael Wyatt <michaelwyatt@protonmail.com>
2019-01-17 10:56:35 -0800

    Added arguments to server.js

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const fs = require("fs");
-const foxTest = require("./foxTest.json");
 const moveNames = require("./src/utils/data/moveNames.json");
 const addNames = require("./addNames");
 const getSpecialNames = require("./getSpecialNames");
 
-const officialMoveNames = moveNames["Fox"];
+const filename = process.argv.slice(2, 3)[0];
+const charName = process.argv.slice(3)[0];
+
+const officialMoveNames = moveNames[charName];
 
 // Replace 0x123 with Special Move Names
-const jsonWithSpecialNames = getSpecialNames(foxTest);
+const jsonWithSpecialNames = getSpecialNames(filename);
 
 // Add Official Move Names to all Moves
 const jsonWithOfficialNames = addNames(officialMoveNames, jsonWithSpecialNames);
 
 // WRITE FILE
 const stringifyData = JSON.stringify(jsonWithOfficialNames);
-fs.writeFile("Fox.framedata.json", stringifyData, err => {
+fs.writeFile(`${charName}.framedata.json`, stringifyData, err => {
   if (err) throw err;
   console.log("The file has been saved.");
 });
+
+/**
+ * Run program with
+ * node server.js filename charName
+ */
